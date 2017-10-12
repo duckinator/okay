fae332da7543c46659ffa6067d68b6420c296ba2
Ellen Marie Dash <me@duckie.co>
2017-10-12 18:28:34 -0400

    wooooooooooo

--- lib/okay/http.rb
+++ lib/okay/http.rb
@@ -7,42 +7,73 @@ class Okay
 
     DEFAULT_REDIRECT_LIMIT = 10
 
-    # A mapping of HTTP request methods to Net::HTTP methods.
-    # (Yay overlapping terminology.)
+    # Make an HTTP GET request.
     #
-    # Also, what are consistent APIs even?
-    METHODS = {
-      get:  'get_response',
-      post: 'post_form',
-    }
-
-    def self.get(url, parameters = {})
-      send_request(METHODS[:get], url, parameters)
+    # @param url [String] The URL to request.
+    # @param parameters [Hash] A hash representing a query string.
+    def self.get(url, parameters: {})
+      send_request(:Get, url, parameters, nil)
     end
 
-    def self.post(url, parameters = {})
-      send_request(METHODS[:post], url, parameters)
+    # Make an HTTP POST request.
+    #
+    # @param url [String] The URL to request.
+    # @param data [String] Raw data to for the body of the POST request.
+    # @param form_data [Hash] Form data, treated as though.
+    def self.post(url, data: nil, form_data: nil)
+      if !data.nil? && !form_data.nil?
+        raise ArgumentError, "cannot specify data and form_data arguments simultaneously."
+      end
+
+      if form_data.nil?
+        body = data
+      else
+        body = URI.encode_www_form(form_data)
+      end
+
+      send_request(:Post, url, nil, body)
     end
 
-    def self.send_request(http_method, url, parameters, limit = DEFAULT_REDIRECT_LIMIT)
+    def self.send_request(http_method, url, parameters, body, limit = DEFAULT_REDIRECT_LIMIT)
       if limit <= 0
         raise RedirectLimitError, "request exceeded redirect limit"
       end
 
+      # Convert the URL (a String) into a URI object.
       uri = URI.parse(url)
-      uri.query = URI.encode_www_form(parameters)
+
+      # Set the query string for the request.
+      uri.query = URI.encode_www_form(parameters) unless parameters.nil?
 
       options = {
+        # If the URI starts with "https://", enable SSL/TLS.
         use_ssl: (uri.scheme == 'https')
       }
 
+      # Net::HTTP.start() keeps a connection to the host alive
+      # for all requests that occur inside the block.
       Net::HTTP.start(uri.host, uri.port, options) do |http|
-        response = Net::HTTP.send(http_method, uri)
+        # Get a reference to the class for the specified request type.
+        # E.g., if it's a post request, this returns Net::HTTP::Post.
+        request_class = Net::HTTP.const_get(http_method)
 
+        # Create the request object, but don't send it yet.
+        request  = request_class.new(uri)
+        # Set the request body, if there is one.
+        request.body = body unless body.nil?
+
+        # Send the request, storing the result in +response+.
+        response = http.request(request)
+
+        # Handle responses.
         case response
         when Net::HTTPSuccess
+          # Request succeeded; return the result.
           response
         when Net::HTTPRedirection
+          # Follow a redirect.
+          # Decrements +limit+ while doing so, to avoid redirect loops.
+          request.
           send_request(METHODS[:get], response['location'], parameters, limit - 1)
         else
           # This seemingly-innocent method raises an exception if the request
