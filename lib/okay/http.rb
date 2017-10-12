b72b3256175c2006dc357fbccc377a0e4b474e84
Ellen Marie Dash <me@duckie.co>
2017-10-12 18:45:54 -0400

    mmmmm, comments

--- lib/okay/http.rb
+++ lib/okay/http.rb
@@ -18,8 +18,9 @@ class Okay
     # Make an HTTP POST request.
     #
     # @param url [String] The URL to request.
-    # @param data [String] Raw data to for the body of the POST request.
-    # @param form_data [Hash] Form data, treated as though.
+    # @param data [String] Raw data to for the body of the request.
+    # @param form_data [Hash] Data for the request body, encoded as though it
+    #   were a form.
     def self.post(url, data: nil, form_data: nil)
       if !data.nil? && !form_data.nil?
         raise ArgumentError, "cannot specify data and form_data arguments simultaneously."
@@ -34,8 +35,16 @@ class Okay
       send_request(:Post, url, nil, body)
     end
 
-    def self.send_request(http_method, url, parameters, body, limit = DEFAULT_REDIRECT_LIMIT)
-      if limit <= 0
+    # Helper method for actually creating a request.
+    #
+    # @param http_method [Symbol] A symbol representing the class name for
+    #   +Net::HTTP+. E.g., +:Get+ for GET requests, +:Post+ for POST, etc.
+    # @param url [String] URL for the request.
+    # @param parameters [Hash, nil] Request parameters (for the query string).
+    # @param body [String, nil] Request body.
+    # @param redirect_limit [Numeric] The maximum number of redirects allowed
+    def self.send_request(http_method, url, parameters, body, redirect_limit = DEFAULT_REDIRECT_LIMIT)
+      if redirect_limit <= 0
         raise RedirectLimitError, "request exceeded redirect limit"
       end
 
@@ -57,7 +66,7 @@ class Okay
         # E.g., if it's a post request, this returns Net::HTTP::Post.
         request_class = Net::HTTP.const_get(http_method)
 
-        # Create the request object, but don't send it yet.
+        # Create the request object, but don't send it.
         request  = request_class.new(uri)
         # Set the request body, if there is one.
         request.body = body unless body.nil?
@@ -72,18 +81,17 @@ class Okay
           response
         when Net::HTTPRedirection
           # Follow a redirect.
-          # Decrements +limit+ while doing so, to avoid redirect loops.
-          #
-          # NOTE: This does not handle HTTP 307 correctly, as it always
-          #       changes to a GET request. https://httpstatuses.com/307
-          send_request(:Get, response['location'], parameters, body, limit - 1)
+          # Decrements +redirect_limit+ while doing so, to avoid redirect loops.
+          # NOTE: Does not handle HTTP 307. https://httpstatuses.com/307
+          send_request(:Get, response['location'], parameters, body, redirect_limit - 1)
         else
-          # This seemingly-innocent method raises an exception if the request
-          # isn't successful.
+          # +response.value+ raises an exception if the request isn't successful.
           response.value
         end
       end
     end
+
+    # Make +send_request+ a private method.
     private_class_method :send_request
   end
 end
