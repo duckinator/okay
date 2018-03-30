8d536af5f78546cbd634a80313ad2463307e0f7d
Ellen Marie Dash <me@duckie.co>
2018-03-30 19:12:13 -0400

    Add Okay.default.

---
+++ lib/okay/default.rb
@@ -0,0 +1,17 @@
+require "okay/version"
+
+module Okay
+  module DefaultValue
+    def self.nil?
+      true
+    end
+
+    def self.inspect
+      "default"
+    end
+  end
+
+  def self.default
+    DefaultValue
+  end
+end
