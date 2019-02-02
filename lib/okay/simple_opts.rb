afd294dbabc4f6cc3720ee5896923590bf6a27ef
Ellen Marie Dash <me@duckie.co>
2019-02-02 03:45:52 -0500

    Add SimpleOpts.

---
+++ lib/okay/simple_opts.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "optparse"
+
+##
+# An OptionParser wrapper providing a few convenience functions.
+class SimpleOpts < OptionParser
+  def initialize(*args, defaults: nil)
+    super(*args)
+    @okay_options = defaults || {}
+  end
+
+  # simple(..., :a)
+  # simple(..., :b)
+  #   ==
+  # options = {}
+  # on(...) { |val| options[:a] = val }
+  # on(...) { |val| options[:b] = val }
+  def simple(*args)
+    key = args.pop
+    on(*args) { |*x| @okay_options[key] = x[0] }
+  end
+
+  def parse(args)
+    parse!(args.dup)
+    @okay_options
+  end
+end
