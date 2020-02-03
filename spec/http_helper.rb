9944b191e4b3a107f0d0d3b53bb4974efbd41b4c
Ellen Marie Dash <me@duckie.co>
2020-02-03 18:43:04 -0500

    Add bin/httpbin and have CI use httpbin.org.

--- spec/http_helper.rb
+++ spec/http_helper.rb
@@ -1,8 +1,16 @@
 require "okay/http"
 
 def define_helper(name)
+  # TODO: Avoid using the production server on CI.
+  host =
+    if ENV["CI"]
+      "https://httpbin.org"
+    else
+      "http://localhost"
+    end
+
   define_method(name) { |url, *args|
-    Okay::HTTP.send(name, "http://localhost#{url}", *args)
+    Okay::HTTP.send(name, "#{host}#{url}", *args)
   }
 end
 
